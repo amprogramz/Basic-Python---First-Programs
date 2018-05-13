ddb89eeaa67bbf1a4cf0bb55a8bfa76569c6b6b7
Alec McDaugale <amcdaugale@student.cccs.edu>
2018-05-12 20:42:25 -0600

    Minor update to tostring method, shows suit and card in a list.

--- Python/src/test/python/Cards.py
+++ Python/src/test/python/Cards.py
@@ -1,6 +1,7 @@
 '''
 Alec McDaugale
-This is a deck of cards. The plan is to eventually use this to make a gui card game. This is basic concept at this time.
+This is a deck of cards. The plan is to eventually use this to make a gui card game. This is only a basic concept at
+this time.
 '''
 class Cards:
     def __init__(self):
@@ -9,17 +10,18 @@ class Cards:
 
 class Deck:
     def __init__(self):
-        values = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10, 11]
-        cards = ["A", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"]
-        suit = ["Hearts", "Diamonds", "Spades", "Clubs"]
-        self._deck = [suit, cards, values]
+        self._values = (1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10, 11)
+        self._cards = ("A", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A")
+        self._suit = ("Hearts", "Diamonds", "Spades", "Clubs")
+        self._deck = (self._suit, self._cards, self._values)
 
     def get_deck(self):
         return  self._deck
 
     def to_string(self):
-        for val in self._deck:
-            print(val)
+        for index1 in range(4):
+            for index2 in range(14):
+                print(self._suit[index1], self._cards[index2])
 
 
 deck = Deck()
