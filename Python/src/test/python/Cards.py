a20b434348457ca20d78acfcbe4624d3b0d956be
Alec McDaugale <amcdaugale@student.cccs.edu>
2018-05-13 12:09:20 -0600

    Made each card an object for better control.

--- Python/src/test/python/Cards.py
+++ Python/src/test/python/Cards.py
@@ -8,13 +8,15 @@ import random
 class BlackJack:
     def __init__(self):
         self._deck = Deck()
-        self._cards = []
+        self._dealer = Player()
+        self._player = Player()
+        '''self._cards = []
         self._cardsvalue = []
         #self._total
         self._dealercards = []
         self._dealercardsvalue = []
         #self._dealertotal
-        #self.deal_cards(2)
+        #self.deal_cards(2)'''
 
     def deal_card(self, cardset, value):
         num = random.randint(0, 12)
@@ -57,12 +59,39 @@ class BlackJack:
         print("Player")
 
 
+class Player:
+    def __init__(self):
+        self._suit
+        self._cards = []
+        self._value = []
+
+    def set_cards(self, card):
+        self._cards.append(card)
+
+    def get_cards(self):
+        return self._cards
+
+    def set_value(self, value):
+        self._value.append(value)
+
+    def get_value(self):
+        return self._value
+
+    def set_suit(self, suit):
+        self._suit(suit)
+
+    def get_suit(self):
+        return self._suit
+
 class Deck:
     def __init__(self):
         self._values = (1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10)
         self._cards = ("A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K")
-        self._suit = ("Hearts", "Diamonds", "Spades", "Clubs")
-        self._deck = (self._suit, self._cards, self._values)
+        self._suits = ("Hearts", "Diamonds", "Spades", "Clubs")
+        self._deck = []
+        for suit in self._suits:
+            for index in range(len(self._cards)):
+                self._deck.append(Card(suit, self._cards[index], self._values[index]))
 
     def get_deck(self):
         return  self._deck
@@ -77,15 +106,34 @@ class Deck:
         return self._suit[index]
 
     def to_string(self):
-        for index1 in range(4):
-            for index2 in range(13):
-                print(self._suit[index1], self._cards[index2])
+        for index in range(len(self._deck)):
+            print(index, end=" ")
+            self._deck[index].to_string()
 
 
-#deck = Deck()
-#deck.to_string()
-game = BlackJack()
-game.deal_cards(2)
+class Card:
+    def __init__(self, suit, card, value):
+        self._value = value
+        self._card = card
+        self._suit = suit
 
-game.to_string()
+    def get_value(self):
+        return self._value
+
+    def get_card(self):
+        return self._card
+
+    def get_suit(self):
+        return self._suit
+
+    def to_string(self):
+        print(self.get_suit(), self.get_card(), self.get_value())
+
+
+deck = Deck()
+deck.to_string()
+#game = BlackJack()
+#game.deal_cards(2)
+
+#game.to_string()
 
