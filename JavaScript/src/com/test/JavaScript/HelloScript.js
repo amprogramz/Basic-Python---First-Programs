eb7a9080fd464d10ec0de4491ed02f6f0141f5cd
Alec McDaugale <amcdaugale@student.cccs.edu>
2018-05-20 23:22:53 -0600

    Added some more basics.

---
+++ JavaScript/src/com/test/JavaScript/HelloScript.js
@@ -0,0 +1,13 @@
+function external1() {
+    document.getElementById('innerScript').innerHTML = "External hello."
+}
+function externalDocumentWriteEx()
+{
+    document.write("Document.write has the potential to delete everything.")
+}
+function virus(){
+    window.alert("Your computer has been infected!")
+}
+function consoleOutput() {
+    console.log("Hello World")
+}
